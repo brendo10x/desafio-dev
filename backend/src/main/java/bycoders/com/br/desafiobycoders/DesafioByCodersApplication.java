81502b415a7d3c9b8b73f236f05d638049a9e4c6
Brendo Arcanjo <brendo10x@gmail.com>
2021-12-03 16:10:27 -0300

    Adicionando projeto spring-boot

---
+++ backend/src/main/java/bycoders/com/br/desafiobycoders/DesafioByCodersApplication.java
@@ -0,0 +1,13 @@
+package bycoders.com.br.desafiobycoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioByCodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioByCodersApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/bycoders/com/br/desafiobycoders/DesafioByCodersApplicationTests.java
@@ -0,0 +1,13 @@
+package bycoders.com.br.desafiobycoders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioByCodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
