1f2700416afb17206d2857b906a05fb8e76e00ff
Brendo Arcanjo <brendo10x@gmail.com>
2021-12-04 07:52:46 -0300

    adding the resource package

---
+++ backend/src/main/java/bycoders/com/br/desafiobycoders/resources/StoreResource.java
@@ -0,0 +1,13 @@
+package bycoders.com.br.desafiobycoders.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StoreResource {
+
+	// POST importar arquivo cnab
+	
+	// GET all lojas
+	
+	// GET all transações por loja
+}
