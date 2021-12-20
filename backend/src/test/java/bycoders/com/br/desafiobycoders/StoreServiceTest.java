ab6d430b2cb5ae8972ef607bf588ff7b89620ab0
Brendo Arcanjo <brendo10x@gmail.com>
2021-12-19 22:05:01 -0300

    refactoring

--- backend/src/test/java/bycoders/com/br/desafiobycoders/StoreServiceTest.java
+++ backend/src/test/java/bycoders/com/br/desafiobycoders/StoreServiceTest.java
@@ -3,7 +3,7 @@ package bycoders.com.br.desafiobycoders;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,37 +11,28 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseTearDown;
-
 import bycoders.com.br.desafiobycoders.entities.Store;
-import bycoders.com.br.desafiobycoders.entities.Transaction;
 import bycoders.com.br.desafiobycoders.repositories.StoreRepository;
-import bycoders.com.br.desafiobycoders.repositories.TransactionCategoryRepository;
-import bycoders.com.br.desafiobycoders.repositories.TransactionRepository;
 import bycoders.com.br.desafiobycoders.services.StoreService;
 
-@SpringBootTest
-@DatabaseTearDown
-@TestExecutionListeners({ TransactionalTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
-		DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class})
-@Transactional
-@TestPropertySource(locations = "/application-test.properties")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = Replace.ANY)
 class StoreServiceTest {
 
 	@Autowired
@@ -49,16 +40,9 @@ class StoreServiceTest {
 
 	@Autowired
 	private StoreRepository storeRepository;
-
-	@Autowired
-	private TransactionRepository transactionRepository;
-
-	@Autowired
-	private TransactionCategoryRepository TransactionCategoryRepository;
-
+ 
 	@BeforeEach
 	 void setUp() {
-
 		importFileCNAB();
 	}
 
@@ -77,10 +61,7 @@ class StoreServiceTest {
 
 		Store store = storeRepository.findByName("BAR DO JOÃO").get();
 
-		loadTransactionsByStore(store);
-
 		assertNotNull(store);
-
 		assertThat(store.getTransactions(), hasSize(3));
 	}
 
@@ -90,25 +71,10 @@ class StoreServiceTest {
 
 		Store store = storeRepository.findByName("MERCADO DA AVENIDA").get();
 
-		loadTransactionsByStore(store);
-
 		assertThat(store.getBalance(), is(new BigDecimal("489.20")));
 	}
 
-	private Store loadTransactionsByStore(Store store) {
 
-		List<Transaction> transactions = transactionRepository.findAllTransactionsByStore(store);
-
-		transactions.stream().forEach(t -> {
-
-			t.setTransactionCategory(TransactionCategoryRepository.findById(t.getTransactionCategory().getId()).get());
-		});
-
-		store.setTransactions(transactions);
-
-		return store;
-	}
- 
 	private void importFileCNAB() {
 
 		MultipartFile multipartFile = null;
