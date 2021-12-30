5404f4451d6d27825b560a873faf8c341bba9cd0
Brendo Arcanjo <brendo10x@gmail.com>
2021-12-30 14:57:05 -0300

    add test

---
+++ backend/src/test/java/bycoders/com/br/desafiobycoders/resources/StoreResourceTest.java
@@ -0,0 +1,19 @@
+package bycoders.com.br.desafiobycoders.resources;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StoreResourceTest {
+
+	@Test
+	final void testUploadFileCNAB() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFindAllStories() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
